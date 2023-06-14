6f62c5e669798d138e5f8242059f136ed952ac2e
Rocamr <100881982+Rocamr@users.noreply.github.com>
2023-06-14 08:08:11 -0600

    Agregar archivos de proyecto.

---
+++ FaceRCA/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace FaceRCA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GrabarVideo = new System.Windows.Forms.Button();
+            this.DetenerVideo = new System.Windows.Forms.Button();
+            this.Estadistica = new System.Windows.Forms.Button();
+            this.NombreV = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.procesosEN = new System.Windows.Forms.ComboBox();
+            this.Refresco = new System.Windows.Forms.Button();
+            this.VideoV = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.VideoV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GrabarVideo
+            // 
+            this.GrabarVideo.Location = new System.Drawing.Point(591, 90);
+            this.GrabarVideo.Name = "GrabarVideo";
+            this.GrabarVideo.Size = new System.Drawing.Size(109, 23);
+            this.GrabarVideo.TabIndex = 0;
+            this.GrabarVideo.Text = "Grabar video";
+            this.GrabarVideo.UseVisualStyleBackColor = true;
+            this.GrabarVideo.Click += new System.EventHandler(this.GrabarVideo_Click);
+            // 
+            // DetenerVideo
+            // 
+            this.DetenerVideo.Location = new System.Drawing.Point(591, 119);
+            this.DetenerVideo.Name = "DetenerVideo";
+            this.DetenerVideo.Size = new System.Drawing.Size(109, 23);
+            this.DetenerVideo.TabIndex = 1;
+            this.DetenerVideo.Text = "Detener Grabacion ";
+            this.DetenerVideo.UseVisualStyleBackColor = true;
+            this.DetenerVideo.Click += new System.EventHandler(this.DetenerVideo_Click);
+            // 
+            // Estadistica
+            // 
+            this.Estadistica.Location = new System.Drawing.Point(706, 90);
+            this.Estadistica.Name = "Estadistica";
+            this.Estadistica.Size = new System.Drawing.Size(105, 43);
+            this.Estadistica.TabIndex = 2;
+            this.Estadistica.Text = "Ver estadisticas de las Grabacion";
+            this.Estadistica.UseVisualStyleBackColor = true;
+            this.Estadistica.Click += new System.EventHandler(this.Estadistica_Click);
+            // 
+            // NombreV
+            // 
+            this.NombreV.Location = new System.Drawing.Point(591, 56);
+            this.NombreV.Name = "NombreV";
+            this.NombreV.Size = new System.Drawing.Size(220, 20);
+            this.NombreV.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(592, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nombre de la grabacion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(592, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Analisis del video ";
+            // 
+            // procesosEN
+            // 
+            this.procesosEN.FormattingEnabled = true;
+            this.procesosEN.Location = new System.Drawing.Point(595, 13);
+            this.procesosEN.Name = "procesosEN";
+            this.procesosEN.Size = new System.Drawing.Size(121, 21);
+            this.procesosEN.TabIndex = 9;
+            this.procesosEN.SelectedIndexChanged += new System.EventHandler(this.procesosEN_SelectedIndexChanged);
+            // 
+            // Refresco
+            // 
+            this.Refresco.Image = global::FaceRCA.Properties.Resources.re2;
+            this.Refresco.Location = new System.Drawing.Point(731, 3);
+            this.Refresco.Name = "Refresco";
+            this.Refresco.Size = new System.Drawing.Size(41, 38);
+            this.Refresco.TabIndex = 10;
+            this.Refresco.UseVisualStyleBackColor = true;
+            this.Refresco.Click += new System.EventHandler(this.Refresco_Click);
+            // 
+            // VideoV
+            // 
+            this.VideoV.Location = new System.Drawing.Point(1, 2);
+            this.VideoV.Name = "VideoV";
+            this.VideoV.Size = new System.Drawing.Size(584, 448);
+            this.VideoV.TabIndex = 4;
+            this.VideoV.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(595, 162);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 288);
+            this.textBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 466);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Refresco);
+            this.Controls.Add(this.procesosEN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NombreV);
+            this.Controls.Add(this.VideoV);
+            this.Controls.Add(this.Estadistica);
+            this.Controls.Add(this.DetenerVideo);
+            this.Controls.Add(this.GrabarVideo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.VideoV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GrabarVideo;
+        private System.Windows.Forms.Button DetenerVideo;
+        private System.Windows.Forms.Button Estadistica;
+        private System.Windows.Forms.PictureBox VideoV;
+        private System.Windows.Forms.TextBox NombreV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox procesosEN;
+        private System.Windows.Forms.Button Refresco;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FaceRCA/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace FaceRCA
+{
+
+    public partial class Form1 : Form
+    {// Importa las funciones de la Windows API necesarias
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr SelectObject(IntPtr hdc, IntPtr hgdiobj);
+
+        [DllImport("gdi32.dll")]
+        public static extern bool BitBlt(IntPtr hdcDest, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hdcSrc, int nXSrc, int nYSrc, uint dwRop);
+
+        [DllImport("user32.dll")]
+        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hdc);
+
+        [DllImport("gdi32.dll")]
+        public static extern int DeleteObject(IntPtr hObject);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out Rectangle lpRect);
+
+        // Estructura para almacenar las coordenadas de la ventana
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        Dictionary<string, Process> rutas = new Dictionary<string, Process>();
+        Process[] allRunningPrograms;
+        Boolean continuar=false;
+        Boolean respuesta = false;
+        // Configuración de la API de Face de Azure
+        string endpoint = "https://apirf.cognitiveservices.azure.com/";
+        string subscriptionKey = "42d7ce1aa2e1405c823a57fc148ee358";
+        public Form1()
+        {
+            InitializeComponent();
+            ProgramasE();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ProgramasE()
+        {
+            allRunningPrograms = Process.GetProcessesByName("Zoom");
+            if (allRunningPrograms.Length > 0) {
+                procesosEN.Items.Add(allRunningPrograms[0].ProcessName);
+                rutas.Add(allRunningPrograms[0].ProcessName, allRunningPrograms[0]);
+            }
+            allRunningPrograms = Process.GetProcessesByName("Discord");
+            if (allRunningPrograms.Length > 0)
+            {
+                procesosEN.Items.Add(allRunningPrograms[0].ProcessName);
+                rutas.Add(allRunningPrograms[0].ProcessName, allRunningPrograms[0]);
+            }
+            allRunningPrograms = Process.GetProcessesByName("msteams");
+            if (allRunningPrograms.Length > 0)
+            {
+                procesosEN.Items.Add("Teams");
+                rutas.Add("Teams", allRunningPrograms[0]);
+            }
+        }
+
+        private void GrabarVideo_Click(object sender, EventArgs e)
+        {
+            continuar = true;
+            if (procesosEN.Text == "Zoom")
+            {
+               Foto("Zoom Reunión");
+               //AnalisisAsync();
+            }
+            else if (procesosEN.Text == "Discord")
+            {
+                Foto("Discord");
+            }
+            else if (procesosEN.Text == "Teams")
+            {
+                MessageBox.Show("Desarrollo");
+            }
+        }
+        private void Refresco_Click(object sender, EventArgs e)
+        {
+            procesosEN.Items.Clear();
+            rutas.Clear();
+            ProgramasE();
+        }
+        static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            Bitmap resizedImage = new Bitmap(width, height);
+            using (Graphics graphics = Graphics.FromImage(resizedImage))
+            {
+                graphics.DrawImage(image, 0, 0, width, height);
+            }
+            return resizedImage;
+        }
+
+        static IntPtr FindWindowByTitle(string title)
+        {
+            IntPtr hWnd = IntPtr.Zero;
+            foreach (Process process in Process.GetProcesses())
+            {
+                if (process.MainWindowTitle.Contains(title)|| process.MainWindowTitle.Contains("Controles de reunión"))
+                {
+                    hWnd = process.MainWindowHandle;
+                    break;
+                }
+            }
+            return hWnd;
+        }
+        // Importa la función de la Windows API para buscar una ventana por su clase y nombre de ventana
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        private void procesosEN_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        private void Foto(string x)
+        {
+            IntPtr hWnd = FindWindowByTitle(x);
+
+            if (hWnd != IntPtr.Zero)
+            {
+                // Obtiene el tamaño de la ventana de llamada de Zoom
+                Rectangle windowRect;
+                GetWindowRect(hWnd, out windowRect);
+                int width = windowRect.Width;
+                int height = windowRect.Height;
+
+                // Captura la pantalla en un bitmap
+                Bitmap bitmap = new Bitmap(width, height);
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    graphics.CopyFromScreen(windowRect.Left, windowRect.Top, 0, 0, new Size(width, height));
+                }
+
+                // Redimensiona la imagen para que se ajuste al tamaño del PictureBox
+                Bitmap resizedBitmap = ResizeImage(bitmap, VideoV.Width, VideoV.Height);
+
+                // Asigna la imagen redimensionada al PictureBox
+                VideoV.Image = resizedBitmap;
+
+                Console.ReadLine();
+                respuesta = true;
+            }
+            else
+            {
+                MessageBox.Show("NO hay una llamada activa");  
+            }
+        }
+        private void DetenerVideo_Click(object sender, EventArgs e)
+        {
+            continuar = false;
+        }
+        static byte[] ImageToByteArray(Image image)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                image.Save(memoryStream, ImageFormat.Jpeg);
+                return memoryStream.ToArray();
+            }
+        }
+        private async Task AnalisisAsync()
+        {
+            // Convierte la imagen en un arreglo de bytes
+            byte[] imageBytes = ImageToByteArray(VideoV.Image);
+            FaceClient faceClient = new FaceClient(new ApiKeyServiceClientCredentials(subscriptionKey));
+            faceClient.Endpoint = endpoint;
+
+            // Realiza la llamada a la API para analizar la imagen
+            try
+            {
+                IList<DetectedFace> detectedFaces = await faceClient.Face.DetectWithStreamAsync(new MemoryStream(imageBytes), returnFaceAttributes: new List<FaceAttributeType?> { FaceAttributeType.Age, FaceAttributeType.Gender, FaceAttributeType.Emotion });
+
+                // Muestra la información de cada rostro detectado
+                foreach (DetectedFace face in detectedFaces)
+                {
+                    textBox1.AppendText($"ID del rostro: {face.FaceId}");
+                    textBox1.AppendText($"Género: {face.FaceAttributes.Gender}");
+                    textBox1.AppendText($"Edad: {face.FaceAttributes.Age}");
+
+                    // Muestra las emociones del rostro
+                    Console.WriteLine("Emociones:");
+                    foreach (KeyValuePair<string, double> emotion in face.FaceAttributes.Emotion.ToRankedList())
+                    {
+                        textBox1.AppendText($"{emotion.Key}: {emotion.Value}");
+                    }
+                }
+            }
+            catch (APIErrorException ex)
+            {
+                MessageBox.Show($"Error al llamar a la API de Face: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void Estadistica_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Desarrollo");
+        }
+    }
+}
